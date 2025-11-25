29f875d75caba9ff6b13930dd3867af3eab97e46
VoidedSet <ksdoshi100@gmail.com>
2025-11-25 12:54:22 +0530

    Removed node modules testing backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/user.routes.js";
 import groupRoutes from "./routes/group.routes.js";
 import goalRoutes from "./routes/goal.routes.js";
 import transactionRoutes from "./routes/transaction.routes.js";
+import moodRoutes from "./routes/mood.routes.js";
+import leaderboardRoutes from "./routes/leaderboad.routes.js";
 
 const app = express();
 app.use(cors());
@@ -15,6 +17,8 @@ app.use("/user", userRoutes);
 app.use("/groups", groupRoutes);
 app.use("/goals", goalRoutes);
 app.use("/transactions", transactionRoutes);
+app.use("/mood", moodRoutes);
+app.use("/leaderboard", leaderboardRoutes);
 
 app.get("/", (req, res) => res.send("API Running"));
 
